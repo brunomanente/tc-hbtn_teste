52edff206988ce8d37620b0d495fbb713d052215
brunomanente <bruno.manente0@gmail.com>
2022-03-07 13:02:49 -0300

    projeto email

---
+++ 0x00/email/Pessoa.java
@@ -0,0 +1,16 @@
+
+public class Pessoa {
+	private String emal;
+	
+	public static boolean emailValid(String emal) {
+		
+		if ((emal.contains("@") || emal.length() <= 50)){
+			return true;
+		}
+			else {
+					return false;
+			}
+		
+	}
+
+}
